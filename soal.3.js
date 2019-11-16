50eb2669f73a5bb7de19f917ae53d8e7eefc73bf
mochnurreza <57040391+mochnurreza@users.noreply.github.com>
2019-11-16 16:01:30 +0700

    Add files via upload

---
+++ soal.3.js
@@ -0,0 +1,7 @@
+document.write("--Ppanjang--<br>");
+    for (var i=1;i<=5;i++){
+        for (var j=1;j<=5;j++){
+            document.write("*");
+        }
+        document.write("<br>");
+    }
---
+++ soal1.js
@@ -0,0 +1,8 @@
+var nilai = new Array(14,13,12,11,10);
+var hasilMax = Math.max.apply(null,nilai);
+var hasilMin = Math.min.apply(null,nilai);
+
+document.write("Nilai : "+ nilai+ "<br>");
+document.write("Nilai Max: "+ hasilMax+ "<hr>");
+document.write("Nilai : "+ nilai+ "<br>");
+document.write("Nilai Min: "+ hasilMin+ "<hr>");
